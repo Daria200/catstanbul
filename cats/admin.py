90c8c4262e300f6abde4138053ba33a722f974e9
Daria Neznaeva <darianeznaeva@gmail.com>
2022-09-28 16:52:30 +0200

    customizing admin display data

--- cats/admin.py
+++ cats/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Cat
 
-# Register your models here.
-admin.site.register(Cat)
+
+class CatAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "is_published", "donation", "volunteer")
+    list_display_links = ("id", "name")
+    list_filter = ("volunteer",)
+    list_editable = ("is_published",)
+    search_fields = ("name", "description", "address", "city", "donation")
+    list_per_page = 25
+
+
+admin.site.register(Cat, CatAdmin)
--- volunteers/admin.py
+++ volunteers/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Volunteer
 
-# Register your models here.
-admin.site.register(Volunteer)
+
+class VolunteerAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "is_vom", "phone", "email")
+    list_display_links = ("id", "name")
+    list_editable = ("is_vom",)
+    search_fields = ("name", "description", "phone", "email")
+    list_per_page = 25
+
+
+admin.site.register(Volunteer, VolunteerAdmin)
