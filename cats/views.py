1ebeb1313dd79351509c11e0add8d86a30f184d0
Daria Neznaeva <darianeznaeva@gmail.com>
2022-09-29 16:44:39 +0200

    pagination

--- cats/views.py
+++ cats/views.py
@@ -1,12 +1,17 @@
 from multiprocessing import context
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 from .models import Cat
 
 
 def cats(request):
-    cats = Cat.objects.all()
-    context = {"cats": cats}
+    cats = Cat.objects.order_by('-list_date').filter(is_published=True)
+
+    paginator = Paginator(cats, 6)
+    page = request.GET.get('page')
+    paged_cats = paginator.get_page(page)
+    context = {"cats": paged_cats}
     return render(request, "cats/cats.html", context)
 
 
