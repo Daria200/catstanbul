b193c80649437cf43f7a6c7b4bff899d2b08c01d
Daria Neznaeva <darianeznaeva@gmail.com>
2022-10-03 19:17:53 +0200

    showing volunteers on page about

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from cats.models import Cat
+from volunteers.models import Volunteer
 
 
 def index(request):
-    cats = Cat.objects.order_by('-list_date').filter(is_published=True)[1:4]
+    cats = Cat.objects.order_by("-list_date").filter(is_published=True)[1:4]
     context = {"cats": cats}
     return render(request, "pages/index.html", context)
 
 
 def about(request):
-    return render(request, "pages/about.html")
+    volunteers = Volunteer.objects.all()
+    context = {"volunteers": volunteers}
+    return render(request, "pages/about.html", context)
