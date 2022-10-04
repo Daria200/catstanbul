4093995aa0c88ebab36f6e97193b37f399c9aa8c
Daria Neznaeva <darianeznaeva@gmail.com>
2022-10-04 11:32:41 +0200

    changing search form so that it options come form dictionaries

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from cats.models import Cat
 from volunteers.models import Volunteer
+from cats.choices import age_choices, gender_choices, donation_choices
 
 
 def index(request):
     cats = Cat.objects.order_by("-list_date").filter(is_published=True)[:3]
-    context = {"cats": cats}
+    context = {
+        "cats": cats,
+        "age_choices": age_choices,
+        "gender_choices": gender_choices,
+        "donation_choices": donation_choices,
+    }
     return render(request, "pages/index.html", context)
 
 
