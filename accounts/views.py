541d02fef6e072a5a1dbc09f2101f15bb3dd0934
Daria Neznaeva <darianeznaeva@gmail.com>
2022-10-21 15:35:41 +0200

    user registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,23 +1,63 @@
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
 
 # Create your views here.
 def register(request):
-    if request.method == 'POST':
-        messages.error(request, 'testing error messages')
-        return redirect('register')
+    if request.method == "POST":
+        # Get form values
+        first_name = request.POST["first_name"]
+        last_name = request.POST["last_name"]
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        password2 = request.POST["password2"]
+
+        # Check if passwords match
+        if password == password2:
+            # Check username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, "That username is taken")
+                return redirect("register")
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, "That email is being used")
+                    return redirect("register")
+                else:
+                    # Good!
+                    user = User.objects.create_user(
+                        username=username,
+                        password=password,
+                        email=email,
+                        first_name=first_name,
+                        last_name=last_name,
+                    )
+                    # Login after register:
+                    # auth.login(request, user)
+                    # messages.success(request, "You are now logged in")
+                    # return redirect("index")
+                    user.save()
+                    messages.success(request, "You are registered and can log in")
+                    return redirect("login")
+
+        else:
+            messages.error(request, "Passwords do not match")
+            return redirect("register")
     else:
-        return render(request, 'accounts/register.html')
+        return render(request, "accounts/register.html")
+
 
 def login(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         # Login User
         pass
     else:
-        return render(request, 'accounts/login.html')
+        return render(request, "accounts/login.html")
+
 
 def logout(request):
-    return redirect('index')
+    return redirect("index")
+
 
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    return render(request, "accounts/dashboard.html")
